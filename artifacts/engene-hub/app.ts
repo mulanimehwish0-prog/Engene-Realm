a198751d43bd91ec54ae21322b7c1d038248b4b7
mulanimehwish0-prog <mulanimehwish.0@gmail.com>
2026-06-21 06:54:53 +0000

    added app typescript

---
+++ artifacts/engene-hub/app.ts
@@ -0,0 +1,35 @@
+import express, { type Express } from "express";
+import cors from "cors";
+import pinoHttp = require("pino-http");
+import router from "./routes";
+import { logger } from "./lib/logger";
+import { Request, Response } from 'express';
+
+const app: Express = express();
+
+app.use(
+  pinoHttp({
+    logger,
+    serializers: {
+      req(req: Request) {
+        return {
+          id: req.id,
+          method: req.method,
+          url: req.url?.split("?")[0],
+        };
+      },
+      res(res: Response) {
+        return {
+          statusCode: res.statusCode,
+        };
+      },
+    },
+  }),
+);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+export default app;
